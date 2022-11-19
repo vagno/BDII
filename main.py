b44ca221390e54d385d53fe2eeb01c30eefa5cfd
vagno <37219434+vagno@users.noreply.github.com>
2022-11-19 14:37:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ arq.close()
 
 #retorna qtd de linhas do log
 n_linhas_log = len(arq_log)
-#print("\nnumero de linhas do codigo: ",n_linhas_log)
+print("\nnumero de linhas do codigo: ",n_linhas_log)
 
 #for i in range(n_linhas_log):
     #print(arq_log[i])
@@ -30,6 +30,7 @@ n_linhas_log = len(arq_log)
 #encontrar commit
 temp = []
 T_commits=[]
+transacao_ckp = []
 for i in range(n_linhas_log-1,0,-1): # linhas de forma decremental
     x = arq_log[i].find("commit") # valor 1 (encontrou), valor -1(não encontrou)
     ckp = arq_log[i].find("CKPT")
@@ -39,7 +40,29 @@ for i in range(n_linhas_log-1,0,-1): # linhas de forma decremental
         #temp = temp.append([i , aux])
         temp = (i, transacao)
         T_commits.append(temp)
-        #print("commit na linha:",i)
+        print("commit na linha:",i)
+
+    temp_ckp = ""
+    if (ckp == 1):
+        for k in range ((len(arq_log[i]) - 7)):
+            j = k + 7
+            if (arq_log[i][j] == "(" or ","):
+                
+                if (arq_log[i][j] != "," and ")"):
+                    temp_ckp += arq_log[i][j]
+                    temp = (i, temp_ckp)
+                    #print("temp_ckp",temp_ckp)
+                
+                if (arq_log[i][j] == ","):
+                    #transacao_ckp.append(temp_ckp)
+                    transacao_ckp.append(temp)
+                    temp_ckp = ""
+                
+                if (arq_log[i][j] == ")"):
+                    transacao_ckp.append(temp) # verificar
+                    #print("terminou chequagem")
+                    break
 
 
+print ("transações do ckp:", transacao_ckp)
 print("transações dos commit:",T_commits)
