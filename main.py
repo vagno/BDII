34ed51da5195ec529543f459255811a53acad459
vagno <37219434+vagno@users.noreply.github.com>
2022-11-20 23:53:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import csv
 
 
 # Leitura arquivo de entrada do Log
-arq = open('entradaLog', 'r')
+arq = open('entradaLog', 'r', encoding="utf_8")
 arq_log = arq.readlines()
 #arq_log = texto.split()
 arq.close() 
@@ -35,12 +35,25 @@ for i in range(n_linhas_log-1,0,-1): # linhas de forma decremental
     x = arq_log[i].find("commit") # valor 1 (encontrou), valor -1(não encontrou)
     ckp = arq_log[i].find("CKPT")
 
-    if (x == 1):
-        transacao = arq_log[i][8] + arq_log[i][9] #transação atual do commit
+
+    #if (x == 1):
+        #transacao = arq_log[i][8] + arq_log[i][9] #transação atual do commit
         #temp = temp.append([i , aux])
-        temp = (i, transacao)
-        T_commits.append(temp)
+        #temp = (i, transacao)
+        #T_commits.append(temp)
+        #print("commit na linha:",i)
+
+    temp_x =""
+    if (x == 1):
+        for l in range(len(arq_log[i])-10):
+            j = l + 8
+            if (arq_log[i][l] != ">"):
+                temp_x += arq_log[i][j]
+                #print("temp_x",temp_x)
         print("commit na linha:",i)
+        temp = (i, temp_x)
+        T_commits.append(temp)
+
 
     temp_ckp = ""
     if (ckp == 1):
@@ -66,3 +79,70 @@ for i in range(n_linhas_log-1,0,-1): # linhas de forma decremental
 
 print ("transações do ckp:", transacao_ckp)
 print("transações dos commit:",T_commits)
+
+# Verificar os commits apos o ckp
+list_redo= [] #lista para deletar
+for i in range(len(transacao_ckp)):
+    for j in range(len(T_commits)):
+        if (T_commits[j][0] > transacao_ckp[i][0]): # transação pode ser excluida no ckp
+            #print("commit",T_commits[j][0])
+            #print("ckp",transacao_ckp[i][0])
+            list_redo.append(T_commits[j][1])
+
+list_redo = sorted(set(list_redo))
+print("list_redo", list_redo)
+
+
+# Função para procedimento do redo
+def realiza_redo(linha):
+    print("linha",linha)
+    # realizar as operações
+    comp_start = arq_log[linha].find("start")
+    comp_CKPT = arq_log[linha].find("CKPT")
+    comp_commit = arq_log[linha].find("commit")
+    comp_crash = arq_log[linha].find("crash")
+
+
+    if comp_start != 1 and comp_CKPT != 1 and comp_commit != 1 and comp_crash != 1:
+        #tem uma operação para verificar necessidade de fazer o redo
+        # list_redo = [ T1 , T4 ]
+        # <start T1>
+        # <T3,2,B,30,1000>
+        # <T1,1,A,20,2000>
+        # < transaçao, linha, variavel, redo, undu>
+        for j in range(len(arq_log[linha])):
+            if j!= 0: #inicia leitura no index 1
+                # encontrar transação atual e verificar se esta na lista do redo
+                temp_x =""
+                if (arq_log[i][l] == ","):
+                    break
+                else:
+                    #temp_x += arq_log[i][j]
+                    #print("temp_x",temp_x)
+                    a=0
+
+        
+
+
+list_seq_redo = [] # para inicial pelo mais antigo (salva a linha)
+# Buscar inicio das transações para realizar o redo
+for j in range(len(list_redo)):
+    for i in range(n_linhas_log-1,0,-1): # linhas de forma decremental
+        linha_start = arq_log[i].find("start " + list_redo[j]) # valor 1 (encontrou), valor -1(não encontrou)
+        
+        if (linha_start == 1):
+            #print("encontrou",arq_log[i])
+            #print("linha", i)
+            list_seq_redo.append(i)
+            break
+
+# Faz sequencia da lista mais antiga
+list_seq_redo.sort()
+
+# passar linhas a partir do inicio do redu
+for i in range(n_linhas_log):
+    realiza_redo(i + list_seq_redo[0]) 
+    if (i + list_seq_redo[0] == n_linhas_log-1):
+        break
+
+#print(T_commits)
